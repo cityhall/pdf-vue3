9f0520403ea83b70a8d437991296a0e2afae6219
christopher <locdt1k5@gmail.com>
2025-07-02 12:44:25 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: 'src/index.ts',
-      name: 'pdf-vue3',
+      name: 'pdf-vue3-f',
       formats: ['es'],
     },
     rollupOptions: {
